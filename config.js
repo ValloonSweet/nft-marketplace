bf32c2df7b1752bb4d00976e586d9497dbf9648d
ValloonSweet <valloon91105@gmail.com>
2022-02-20 11:38:11 +0300

    fix: deploy to mumbai

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
-export const nftmarketaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftaddress = "0x681A9446c84ef978B511cE872BfFC7A2a8063aE0"
+export const nftmarketaddress = "0x736680D21e2B0C63813FEBc4432891579C28EEe8"
